55b97e994b9e21eaab370488d6c6f9a88b099465
Isaac Cohen <isaaclandoncohen@gmail.com>
2022-11-28 19:03:58 -0800

    basic gran working time to play, flatten grain broken

--- Assets/GranularSynthesis.cs
+++ Assets/GranularSynthesis.cs
@@ -34,16 +34,68 @@ public AudioClip clip;
 
     public float clipLength;
     public float grainLength;
+
+
+    public float grainSize;
+    public float grainSizeRandomness;
+    public float grainPlaybackRate;
+    public float grainPlaybackRateRandomness;
+    public float playbackLocation;
+    public float playbackLocationRandomness;
+    public float fadeInSize;
+    public float fadeOutSize;
+
+    public float timeBetweenGrains;
+    public float timeBetweenGrainsRandomness;
     
+
+    int ti2;
+
+    float rand(int v){
+        return (Mathf.Sin((float)v * 14.1455f + 10 * Mathf.Sin((float)v*4141.44f)) + 1)/2;
+
+    }
+
+
+    // where we start playing our sample from ( in sample space)
+    public int[] startTimeInClip;
+ 
+    // how many samples long our clip is
+    public int[] clipLengths;
+
+    // what time our 'sample clock' started playing at.
+    public int[] clipStartPlayTime;
+
+    public float[] playbackSpeed;
+
+    public bool[] toDestroy;
+
+    public int maxGrains = 100;
+    public int currentNumberGrains = 0;
 
     void OnEnable()
     {
+
+        startTimeInClip = new int[maxGrains];
+        clipLengths = new int[maxGrains];
+        clipStartPlayTime = new int[maxGrains];
+        toDestroy = new bool[maxGrains];
+        playbackSpeed = new float[maxGrains];
+
+        for( int i =0 ; i < maxGrains; i++ ){
+            startTimeInClip[i] = 0;
+            clipLengths[i] = 0;
+            clipStartPlayTime[i] = 00;
+            toDestroy[i] = false;
+            playbackSpeed[i] = 1110;
+        }
 
 
 
         //AudioSettings.outputSampleRate = clip.frequency;
         // setting our sample rate
         sampleRate = AudioSettings.outputSampleRate;
+
 
         // getting our full clip data into a data buffer
         totalClipSamples = clip.samples * clip.channels;
@@ -54,142 +106,169 @@ public AudioClip clip;
         started = true;
 
         clipLength = clip.length;
+        timeIndex = 0;
 
 
 
     }
 
 
-    public List<Grain> grains = new List<Grain>();
 
     void OnAudioFilterRead(float[] data , int channels ){
 
 
         if( started ){  
 
+            for(int i = 0; i < data.Length; i+=channels){
+                
+                timeIndex ++;
+                timeAudio = (float)timeIndex / (float)sampleRate;
 
-        //Creates a sinewave
-        for(int i = 0; i < data.Length; i+=channels){
+                for( int j = 0; j< currentNumberGrains; j++ ){
             
-            timeIndex ++;
-            timeAudio = (float)timeIndex / (float)sampleRate;
+                    data[i]   += sampleGrain(j,0);
+                    data[i+1]   += sampleGrain(j,1);
 
-            for( int j = 0; j< grains.Count; j++ ){
-                data[i]   += grains[j].getSample(0);
-                data[i+1]   += grains[j].getSample(0);
+                }
+                
             }
+
             
-        
+            FlattenGrains();
+
+    
+
         }
 
-        for( int i = 0; i < grains.Count; i++ ){
 
-           // print( grains[i].finished );
-            if( grains[i].finished== true ){
-                print("fin");
-                grains.RemoveAt(i--);
+     
+    }
+
+
+
+float fGrainLength = 0;
+void FixedUpdate(){
+
+
+
+            if( timeAudio - lastGrainTime > fGrainLength ){
+                fGrainLength = timeBetweenGrains + Random.Range(-timeBetweenGrainsRandomness,timeBetweenGrainsRandomness);
+                lastGrainTime = timeAudio;
+                MakeNewGrain();
             }
-        }
 
-      
+}
 
+
+float  sampleGrain(int id , int channel){
+
+
+
+
+    int sampleTimePlayed = timeIndex - clipStartPlayTime[id];
+
+    int speedCorrected = (int)((float)sampleTimePlayed * playbackSpeed[id]);
+
+    float nTime = (float)speedCorrected / (float)clipLengths[id];
+    
+    int finalSample = startTimeInClip[id] + (int)((float)sampleTimePlayed * playbackSpeed[id]);
+
+    float v = clipData[finalSample*2+ channel];
+
+    if( speedCorrected > clipLengths[id]){
+        v = 0;
+        toDestroy[id] = true;
+    }
+
+    v *= Mathf.Min(nTime * 3 , 1-nTime);
+
+    return v;
+
+}
+
+
+void FlattenGrains(){
+
+
+
+bool completed = false;
+int i = 0;
+while (completed == false) 
+{
+
+    if( toDestroy[i] == true ){
+
+    }
+    i++;
+
+    if( i == maxGrains ){
+        completed = true;
     }
 
 
-    }
 
-
-    public void FixedUpdate(){
-        if( timeAudio - lastGrainTime > grainLength ){
-            lastGrainTime = timeAudio;
-            grainLength= 1000;//Random.Range( 0,2);
-           grains.Add( new Grain(this) );
-        }
-    }
-
-    public struct Grain{
-        public float startLocation;
-        public float playbackSpeed;
-        public float length;
-        public int   indexInClip;
-        public float timeInClip;
-        public float normalizedTime;
-
-        public int startTimeReference;
-        public bool finished;
-
-        public int startSample;
-        public int baseTime;
-
-        GranularSynthesis synth;
+}
 
 
 
 
-        public Grain(GranularSynthesis s){
-            synth = s;
-            
-            startTimeReference = synth.timeIndex;
-            length = 1;
-            finished = false;
-            startLocation = ((Mathf.Sin( (float)synth.timeIndex ) +1)/2) * ( synth.clipLength - length*2 );
-            startSample = (int)Mathf.Floor( startLocation * synth.sampleRate );
-            playbackSpeed = Random.Range(1f,2f);
-            indexInClip = (synth.timeIndex - startTimeReference)+ startSample;
-            baseTime = (synth.timeIndex - startTimeReference);
-            timeInClip = (float)baseTime/(float)synth.sampleRate; 
-            normalizedTime = timeInClip / length;
+for( int j = 0; j< currentNumberGrains; j++ ){
+          
+    int id = j;
 
+    if( toDestroy[id] == true ){
 
-        }
+            if( id == currentNumberGrains-1){
+                // just destroy
+                toDestroy[id] = false;
+                //clipLengths[id] = 0; 
+                //clipStartPlayTime[id] = 0;
+                //startTimeInClip[id] = 0;
+                //playbackSpeed[id] = 1110;
+            }else{
 
-
-
-        public float getSample(int channel){
-
-
-            baseTime = (synth.timeIndex - startTimeReference);
-
-            int baseIndex = (int)Mathf.Floor(baseTime * playbackSpeed) + startSample;
-            int ceilIndex = (int)Mathf.Ceil(baseTime * playbackSpeed) + startSample;
-
-            float lerpVal = baseTime * playbackSpeed - Mathf.Floor(baseTime * playbackSpeed);
-
-            float d1 = synth.clipData[baseIndex];
-            float d2 = synth.clipData[ceilIndex];
-
-
-            float fVal = d1 + (d2-d1)*lerpVal;
-
-            indexInClip = (int)Mathf.Round(baseTime * playbackSpeed) + startSample;
-
-
-            // gives us our time in our clip so we can fade in and out correctly
-            timeInClip = (float)baseTime/(float)synth.sampleRate;
-            normalizedTime = timeInClip / length;
-
-
-            if( normalizedTime >= 1 ){
-                finished = true;
-                return 0;
+                if( toDestroy[id] == toDestroy[id+1]){
+                    print("SAME");
+                }
+                toDestroy[id] = toDestroy[id+1];
+                //clipLengths[id] = clipLengths[id+1];
+                //clipStartPlayTime[id] = clipStartPlayTime[id+1];
+                //playbackSpeed[id] = playbackSpeed[id+1];
+                //startTimeInClip[id] = startTimeInClip[id+1];
             }
-            
 
-            float vol = Mathf.Min( normalizedTime * 10, (1-normalizedTime) );
-
-            return fVal  * vol;
-
-
-
-
+            currentNumberGrains --;
         }
 
+    }
+}
+
+    void MakeNewGrain(){
+
+        if( currentNumberGrains >= maxGrains ){
+                 
+                    return;
+                }
+
+
+                clipLengths[currentNumberGrains] = (int)((grainSize + Random.Range(-grainSizeRandomness,grainSizeRandomness)) * (float)sampleRate); // just making it 1 second to start!
+
+                playbackSpeed[currentNumberGrains] = grainPlaybackRate + Random.Range(-grainPlaybackRateRandomness,grainPlaybackRateRandomness);
+            
+                clipStartPlayTime[currentNumberGrains] = timeIndex; // start playing now!
+                
+                
+                startTimeInClip[currentNumberGrains] = (int)( (playbackLocation + Random.Range(-playbackLocationRandomness,playbackLocationRandomness)) * (float)sampleRate);// Random.Range( 0, totalClipSamples/2 - (int)((float)clipLengths[currentNumberGrains]*2 * playbackSpeed[currentNumberGrains])); // starting at 0 to test!
+
+                
+                currentNumberGrains ++;
+
 
     }
 
-    public float CreateSine(){
-      return Mathf.Sin(2 * Mathf.PI * timeIndex * frequency / (float)sampleRate);
-    }
+
+
+
 
 
 }
